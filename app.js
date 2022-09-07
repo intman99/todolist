186d4dc97fdc4117f331ee521b7748f4774ab159
David Zoike <divad.ekioz@web.de>
2022-09-07 22:54:08 +0200

    changed color and zoom

--- app.js
+++ app.js
@@ -146,6 +146,6 @@ app.get("/about", function(req, res) {
 
 app.listen(process.env.PORT);
 
-// app.listen(3001, function() {
-//   console.log("Server started on port 3000");
-// });
+app.listen(3001, function() {
+  console.log("Server started on port 3000");
+});
